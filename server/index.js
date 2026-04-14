7cd32650e32ac80a02a931ffb5185c058dee3ddd
Ana Izma <anaizma@Anas-MacBook-Air.local>
2026-04-13 21:35:26 -0400

    Chore: making files and directories

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
